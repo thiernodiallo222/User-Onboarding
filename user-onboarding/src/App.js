7e40c842e74ca6bbd2f5317b3b6e15e561c7b81a
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-01-24 05:32:15 -0500

    just commit

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Form from './Form';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Form />
        
       </header>
     </div>
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Form.scss';
+
+export default function Form() {
+    return (
+        <div>
+            <p>This is the form  using Formik !</p>
+        </div>
+    )
+}
